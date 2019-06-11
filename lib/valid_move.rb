21d3fd121a042182cd468100497151113e3ede36
Chris Cheong <chrisxcheong@gmail.com>
2019-06-11 03:34:44 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,9 +2,6 @@
 def valid_move?(board,index)
   if  index<8 && index>=0 && position_taken?(board,index) == false
     return true
-  elsif index<8 && index>=0
-        position_taken?(board,index) == true
-    return false
   else
     false
 
